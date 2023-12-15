0684861b9f99b32e511dc9a6f63f11ee44644925
DanAun <daniel.aunan@gmail.com>
2023-12-15 14:51:48 +0100

    Finished function stubs

--- src/fordFulkerson.ml
+++ src/fordFulkerson.ml
@@ -1,3 +1,3 @@
 let max_flow graph source sink =
   let list_paths = all_paths_from_source_to_sink graph source sink in
-  List.iter (sub_label min_label ) list_paths
+  List.iter (sub_label min_label i) list_paths
