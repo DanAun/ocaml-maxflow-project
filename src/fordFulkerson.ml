338d619059f49f5b6a6dce450e693b1f5da16b67
EmilieGreaker1 <emigre01@hotmail.com>
2023-12-15 14:54:17 +0100

    Update fordFulkerson.ml

--- src/fordFulkerson.ml
+++ src/fordFulkerson.ml
@@ -1,3 +1,9 @@
+type path = int arc list
+
+
+
+
+
 let max_flow graph source sink =
   let list_paths = all_paths_from_source_to_sink graph source sink in
   List.iter (sub_label min_label i) list_paths
